224aa5fb06f2b4fc167195fb2e6d28fd88eb0c7f
社亦园 <2125107118@qq.com>
2026-03-07 23:47:24 +0800

    refactor: 使用 mdx 重构 about 页面

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
+import mdx from "@astrojs/mdx";
+
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 import swup from "@swup/astro";
-import { defineConfig } from "astro/config";
 import expressiveCode from "astro-expressive-code";
 import icon from "astro-icon";
+import { defineConfig } from "astro/config";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeComponents from "rehype-components"; /* Render the custom directive content */
 import rehypeKatex from "rehype-katex";
@@ -107,6 +109,7 @@ export default defineConfig({
 				showCopyToClipboardButton: false,
 			},
 		}),
+		mdx(),
 		svelte(),
 		sitemap(),
 	],
